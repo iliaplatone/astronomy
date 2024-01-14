8c0d5b67e8a43f333311729da34273d513b1ad2f
Ilia Platone <info@iliaplatone.com>
2024-01-14 12:27:11 +0100

    automatic update 202401141227

--- scripts/plot.sh
+++ scripts/plot.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 catalog=$1
-tmp_csv=/tmp/$$.csv
+tmp_csv=/tmp/cat.csv
 
 cat $catalog | while read line; do
- echo $(echo "$(echo $line | sed -e 's/ /;/g' | cut -d ';'  -f 3)+1" | bc -l)",0" >> $tmp_csv
- echo "$(echo $line | sed -e 's/ /;/g' | cut -d ';'  -f 3),$(echo $line | cut -d ' ' -f 1)" >> $tmp_csv
- echo $(echo "$(echo $line | sed -e 's/ /;/g' | cut -d ';'  -f 3)-1" | bc -l)",0" >> $tmp_csv
+ echo $(echo "$(echo $line | tr -s ' ' ';' | cut -d ';'  -f 3)+1" | bc -l)",0" >> $tmp_csv
+ echo "$(echo $line | tr -s ' ' ';' | cut -d ';'  -f 3),$(echo $line | cut -d ' ' -f 1)" >> $tmp_csv
+ echo $(echo "$(echo $line | tr -s ' ' ';' | cut -d ';'  -f 3)-1" | bc -l)",0" >> $tmp_csv
 done
 
 echo "set datafile separator ','
@@ -16,4 +16,4 @@ set logscale x 1
 set logscale y 1
 plot '$tmp_csv' w lines
 " |gnuplot -persist
-rm $tmp_csv
+#rm $tmp_csv
