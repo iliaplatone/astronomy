c6564102368e0d139069d55e29f2fdb7ef9c6987
Ilia Platone <info@iliaplatone.com>
2024-01-14 12:00:11 +0100

    automatic update 202401141200

--- scripts/plot.sh
+++ scripts/plot.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+catalog=$1
+tmp_csv=$TMPDIR/$$.csv
+
+cat $catalog | while read line; do
+ echo "$(echo $line | cut -s ' ' -f 2),$(echo $line | cut -s ' ' -f 1)" >> $tmp_csv
+done
+
 echo "set datafile separator ','
 set xlabel 'channel'
 set ylabel 'counts'
