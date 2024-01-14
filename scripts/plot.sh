326379ced8a8c82899e8b6d6cffbbc6c4305b761
Ilia Platone <info@iliaplatone.com>
2024-01-14 12:07:11 +0100

    automatic update 202401141207

--- scripts/plot.sh
+++ scripts/plot.sh
@@ -4,7 +4,7 @@ catalog=$1
 tmp_csv=/tmp/cat.csv
 
 cat $catalog | while read line; do
- echo "$(echo $line | cut -d ' ' -f 2),$(echo $line | cut -d ' ' -f 1)" >> $tmp_csv
+ echo "$(echo $line | cut -d ' ' -f 3),$(echo $line | cut -d ' ' -f 1)" >> $tmp_csv
 done
 
 echo "set datafile separator ','
@@ -14,4 +14,4 @@ set logscale x 2
 set logscale y 2
 plot '$tmp_csv' w lines
 " |gnuplot -persist
-#rm $tmp_csv
+rm $tmp_csv
