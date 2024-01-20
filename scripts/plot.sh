6a11911e18e097fe1b3f61a8dfc5176717d28ca2
Ilia Platone <info@iliaplatone.com>
2024-01-20 14:39:11 +0100

    automatic update 202401201439

--- scripts/plot.sh
+++ scripts/plot.sh
@@ -10,17 +10,18 @@ logscale_y=$3
 tmp_csv=/tmp/$$.csv
 
 sed -e 's/ /;/g'  $catalog | while read line; do
+ x_val=$(echo $line | cut -d ';'  -f 3);
+ y_val=$(echo $line | cut -d ';'  -f 1);
  echo $line
- echo $(echo "$(echo $line | cut -d ';'  -f 3)+1" | bc -l)",0" >> $tmp_csv
- echo "$(echo $line | cut -d ';'  -f 3),$(echo 'l($(echo $line | cut -d ' ' -f 1))/l($logscale_y)' | bc -l)" >> $tmp_csv
- echo $(echo "$(echo $line | cut -d ';'  -f 3)-1" | bc -l)",0" >> $tmp_csv
+ echo $(echo "$x_val+1" | bc -l)",0" >> $tmp_csv
+ echo "$x_val,$(echo 'l($y_val)/l($logscale_y)' | bc -l)" >> $tmp_csv
+ echo $(echo "$x_val-1" | bc -l)",0" >> $tmp_csv
 done
 
 echo "set datafile separator ','
 set xlabel 'channel'
 set ylabel 'counts'
 set logscale x $logscale_x
-set logscale y $logscale_y
 plot '$tmp_csv' w lines
 " |gnuplot -persist
 rm $tmp_csv
