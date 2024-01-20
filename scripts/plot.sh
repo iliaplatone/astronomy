d1800a140885be991eced8343503703fb9f4c9d9
Ilia Platone <info@iliaplatone.com>
2024-01-20 14:49:11 +0100

    automatic update 202401201449

--- scripts/plot.sh
+++ scripts/plot.sh
@@ -12,11 +12,13 @@ tmp_csv=/tmp/$$.csv
 sed -e 's/ /;/g'  $catalog | while read line; do
  x_val=$(echo $line | cut -d ';'  -f 3);
  y_val=$(echo $line | cut -d ';'  -f 1);
+ x_mval=$(echo "1.0/($x_val-1)" | bc -l)
  x_val=$(echo "1.0/$x_val" | bc -l)
+ x_pval=$(echo "1.0/($x_val+1)" | bc -l)
  echo $line
- echo $(echo "$x_val+1" | bc -l)",0" >> $tmp_csv
+ echo "$x_mval,0" >> $tmp_csv
  echo "$x_val,$y_val" >> $tmp_csv
- echo $(echo "$x_val-1" | bc -l)",0" >> $tmp_csv
+ echo "$x_pval,0" >> $tmp_csv
 done
 
 echo "set datafile separator ','
