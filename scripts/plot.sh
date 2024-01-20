3fb836a594859956397e4be50412179b27dbf381
Ilia Platone <info@iliaplatone.com>
2024-01-20 14:52:11 +0100

    automatic update 202401201452

--- scripts/plot.sh
+++ scripts/plot.sh
@@ -16,6 +16,9 @@ sed -e 's/ /;/g'  $catalog | while read line; do
  x_pval=$(echo "1.0/($x_val+1)" | bc -l)
  x_val=$(echo "1.0/$x_val" | bc -l)
  y_val=$(echo "sqrt($y_val)" | bc -l)
+ y_val=$(echo "sqrt($y_val)" | bc -l)
+ y_val=$(echo "sqrt($y_val)" | bc -l)
+ y_val=$(echo "sqrt($y_val)" | bc -l)
 
  echo $line
  echo "$x_mval,0" >> $tmp_csv
@@ -27,6 +30,7 @@ echo "set datafile separator ','
 set xlabel 'channel'
 set ylabel 'counts'
 set logscale x $logscale_x
+set logscale y $logscale_y
 plot '$tmp_csv' w lines
 " |gnuplot -persist
 rm $tmp_csv
