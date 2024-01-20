13381e858971118ad1c0d2b6152032a8ad421588
Ilia Platone <info@iliaplatone.com>
2024-01-20 14:47:10 +0100

    automatic update 202401201447

--- scripts/plot.sh
+++ scripts/plot.sh
@@ -12,6 +12,7 @@ tmp_csv=/tmp/$$.csv
 sed -e 's/ /;/g'  $catalog | while read line; do
  x_val=$(echo $line | cut -d ';'  -f 3);
  y_val=$(echo $line | cut -d ';'  -f 1);
+ x_val=$(echo "1.0/$x_val" | bc -l)
  echo $line
  echo $(echo "$x_val+1" | bc -l)",0" >> $tmp_csv
  echo "$x_val,$y_val" >> $tmp_csv
